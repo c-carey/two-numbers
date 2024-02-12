dbacfbac5310c7e87067da5a652fd92c388f9f9e
Cecilia Blumberg <cblumber@iwu.edu>
2024-02-12 09:17:06 -0600

    Adding py file

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+app.route('/')
+def index():
+    render_template("index.html")
+
+app.route('/result')
+def result():
+    first = int(request.args.get('first'))
+    second = int(request.args.get('second'))
+    operand = str(request.args.get('operand'))
+    if operand == "+":
+        result = first + second
+    if operand == "-":
+        result = first - second
+    if operand == "*":
+        result = first * second
+    if operand == "/" and second != 0:
+        result = first / second
+    if operand == "/" and second == 0:
+        result = "Division by zero not supported"
+    return render_template("result.html", result=result, first=first, second=second, operand=operand)
+
